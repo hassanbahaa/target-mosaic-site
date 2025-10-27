b4a21cb7b5015135b6bf62f33920a77ce215931f
Hassan Bahaa <66232742+hassanbahaa@users.noreply.github.com>
2025-10-27 21:43:57 +0300

    Update Booking.com logo path in PlatformsSection

--- src/components/PlatformsSection.tsx
+++ src/components/PlatformsSection.tsx
@@ -3,12 +3,12 @@ import {
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel";
-import bookingLogo from '../assets/websites/booking.png';
+
 
 import AutoScroll from "embla-carousel-auto-scroll";
 
 const platforms = [
-  { name: "Booking.com", logo: "{bookingLogo}" },
+  { name: "Booking.com", logo: "../assets/websites/booking.png" },
   { name: "Expedia", logo: "https://logo.clearbit.com/expedia.com" },
   { name: "Agoda", logo: "https://logo.clearbit.com/agoda.com" },
   { name: "TripAdvisor", logo: "https://logo.clearbit.com/tripadvisor.com" },
